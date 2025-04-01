b3eca849726dc3b2f70e92c2fe37b550fe18794c
ofek legit <ofek@legitsecurity.com>
2025-04-01 19:14:25 +0300

    Add automatic database initialization

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+# Create tables at startup
+with app.app_context():
+    db.create_all()
+
 # Database Models
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
